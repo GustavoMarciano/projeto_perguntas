b7fad6180cbe87fa795c528e7045b7916d3ad793
Gustavo Marciano <gustavodexpace@gmail.com>
2022-09-16 19:19:41 -0300

    Pergunta e botoes
    Pergunta e botoes

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,42 @@ void main() {
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({super.key});
 
+  final perguntaSelecionada = 0;
+
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Perguntas'),
+          title: const Center(child: Text('🤔Perguntas')),
         ),
-        body: const Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta3'),
+            ),
+          ],
+        ),
       ),
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
